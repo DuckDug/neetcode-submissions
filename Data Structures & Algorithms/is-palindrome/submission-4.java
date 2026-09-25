a2e4cec0f3ba6d6bb79981803056f92ba764b2ae
Jose <61916488+DuckDug@users.noreply.github.com>
2026-09-25 09:58:53 -0500

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int l = 0;
+        int r = s.length() - 1;
+
+        while (l < r) {
+            while (l < r && !isAlpha(s.charAt(l))) {
+                l++;
+            }
+            while(l < r && !isAlpha(s.charAt(r))) {
+                r--;
+            }
+
+            char lChar = Character.toLowerCase(s.charAt(l));
+            char rChar = Character.toLowerCase(s.charAt(r));
+
+            if (lChar != rChar) {
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+
+    public boolean isAlpha(char c) {
+        return 'a' <= c && c <= 'z' ||
+            'A' <= c && c <= 'Z' ||
+            '0' <= c && c <= '9';
+    }
+}
